c2dc76e93afba58453d93d6165316685e1b8e15f
Filmon <fabraha@yearup.org>
2022-10-23 00:29:15 -0500

    completed

--- src/main/kotlin/week05/rockPaperScissors.kt
+++ src/main/kotlin/week05/rockPaperScissors.kt
@@ -1,97 +1,54 @@
-/*fun main() {
-    val rock = 0
-    val paper = 1
-    val scissor = 2
-    println("Enter 0,1, or 2 to start the game: ")
-    println("player 1: ")
-    var player1 = readLine()!!.toInt()
-    println("player 2: ")
-    var player2 = readLine()!!.toInt()
-    }
- */
+package week05
+fun main(){
+
+    var options = arrayOf("Rock", "Paper","Scissors")
+    var gameChoice = getGameChoice(options)
+
+    var userOptions = getUserOption(options)
+
+    printResult(gameChoice, userOptions)
 
 
-    import java.util.Scanner
+}
+fun getGameChoice(optionParam: Array<String>)= optionParam[(Math.random()*optionParam.size).toInt()]
 
-    fun rand(start: Int, end: Int): Int {
-        require(start <= end) { "Invalid" }
-        return (start..end).random()
-    }
+fun getUserOption(optionParam: Array<String>):String{
+    var isValid = false
+    var userChoice = ""
 
-    fun main() {
-        var start = 1
-        var end = 3
-        var CPU = rand(start, end)
-        val scan = Scanner(System.`in`)
-        var HUMAN:Int = scan.nextInt()
-        println("Enter 1 for rock, 2 for paper and 3 for scissors: ")
-        println("You:" + HUMAN)
-        println("CPU:" + CPU)
-        if (CPU == 1) {
-            if (HUMAN == 1)
-            {
-                println("CPU choses rock!")
-                println("It is a tie!")
-            }
-            else if (HUMAN == 2)
-            {
-                println("CPU choses paper!")
-                println("It is a tie!")
+    while (!isValid) {
+        println("Enter a valid choice")
 
-            }
-            else if (HUMAN == 3)
-            {
-                println("CPU choses scissors!")
-                println("It is a tie!")
-            }
-            else
-            {
-                println("Wrong option! Type 1 for rock, 2 for paper and 3 for scissors!")
-            }
-        }
-        else if (CPU == 2) {
-            if (HUMAN == 1)
-            {
-                println("CPU choses paper!")
-                println("Game over! Paper beats rock!")
+        for (items in optionParam) println(items)
+        println(".")
 
-            }
-            else if (HUMAN == 2)
-            {
-                println("CPU choses scissors!")
-                println("Game over! Scissors beat paper!")
+        var userInput = readLine()
 
-            }
-            else if (HUMAN == 3)
-            {
-                println("CPU choses rock!")
-                println("Game over! Rock beats scissors!")
-            }
-            else
-            {
-                println("YWrong option! Type 1 for rock, 2 for paper and 3 for scissors!")
-            }
-        }
-        else if (CPU == 3) {
-            if (HUMAN == 1) {
-                println("CPU choses scissors!")
-                println("You win! You got luck from this game! Rock beats scissors!")
+        if (userInput != null && userInput in optionParam){
+            isValid =  true
 
-            }
-            else if (HUMAN == 2) {
-                println("CPU choses rock!")
-                println("You win! You got luck from this game! Paper beats rock!")
-            }
-            else if (HUMAN == 3) {
-                println("CPU choses paper!")
-                println("You win! You got luck from this game! Scissors beat paper!")
-
-            }
-            else {
-                println("Invalid choice! Type 1 for rock, 2 for paper and 3 for scissors!")
-
-            }
-
+            userChoice = userInput.capitalize()
         }
 
+        if (!isValid)
+            println(" please enter a valid choice")
     }
+    return userChoice
+}
+
+fun printResult(gameChoice: String, userChoice: String){
+
+    var result: String
+    if (gameChoice==userChoice) result="tie"
+
+    else if ((gameChoice== "Scissors" && userChoice=="Paper")||
+        (gameChoice== "Rock" && userChoice=="Scissors")||
+        (gameChoice== "Paper" && userChoice=="Rock"))
+
+        result = "You lost!"
+
+    else
+        result = "You Win!"
+
+    println("The game choice $gameChoice and you chose $userChoice . The result is $result" )
+}
